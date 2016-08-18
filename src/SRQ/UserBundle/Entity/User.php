d1457ab48d9a735b243f21064b175682453bfd53
franticzero <franticzero@gmail.com>
2016-08-18 05:01:18 +0000

    Se añaden validaciones al formulario de creacion de usuario

--- src/SRQ/UserBundle/Entity/User.php
+++ src/SRQ/UserBundle/Entity/User.php
@@ -5,6 +5,7 @@ namespace SRQ\UserBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 
 /**
@@ -12,6 +13,10 @@ use Symfony\Component\Validator\Constraints as Assert;
  *
  * @ORM\Table(name="users")
  * @ORM\Entity(repositoryClass="SRQ\UserBundle\Repository\UserRepository")
+ * @UniqueEntity("username"){
+     message="Este usuario ya existe en la base de datos."
+ }
+ * @UniqueEntity("email")
  * @ORM\HasLifecycleCallbacks()
  */
 class User implements UserInterface
@@ -37,7 +42,7 @@ class User implements UserInterface
      * @var string
      *
      * @ORM\Column(name="first_name", type="string", length=100)
-     * 
+     * @Assert\NotBlank()
      */
     private $firstName;
 
@@ -45,6 +50,7 @@ class User implements UserInterface
      * @var string
      *
      * @ORM\Column(name="last_name", type="string", length=100)
+     * @Assert\NotBlank()
      */
     private $lastName;
 
@@ -52,6 +58,8 @@ class User implements UserInterface
      * @var string
      *
      * @ORM\Column(name="email", type="string", length=100)
+     * @Assert\NotBlank()
+     * @Assert\Email()
      */
     private $email;
 
@@ -59,6 +67,7 @@ class User implements UserInterface
      * @var string
      *
      * @ORM\Column(name="password", type="string", length=255)
+     * @Assert\NotBlank()
      */
     private $password;
 
@@ -66,6 +75,8 @@ class User implements UserInterface
      * @var string
      *
      * @ORM\Column(name="role", type="string", columnDefinition="ENUM('ROLE_ADMIN', 'ROLE_USER')",length=50)
+     * @Assert\NotBlank()
+     * @Assert\Choice(choices = {"ROLE_ADMIN", "ROLE_USER"})
      */
     private $role;
 
